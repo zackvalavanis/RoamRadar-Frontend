9bea7455b8cb283c8a094d231ff6c11a9f415e48
Zack <168221270+Zval321@users.noreply.github.com>
2024-11-25 20:12:39 -0600

    added index action with axios call to cities page

--- src/CitiesPage/CitiesPage.jsx
+++ src/CitiesPage/CitiesPage.jsx
@@ -1,8 +1,25 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+
 export function CitiesPage () { 
+  
+
+  const handleIndex = async () => {
+    console.log("handleIndex");
+    try { 
+      const response = await axios.get('http://localhost:3000/cities.json')
+      console.log(response.data)
+    } catch(error) { 
+      console.error("Error fetching cities", error)
+    }
+  }
+
+  useEffect(() => (handleIndex()), []);
+
   return ( 
     <div>
       <h1>
-        Hello
+        Your Cities
       </h1>
     </div>
   )
