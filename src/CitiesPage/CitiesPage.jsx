be97c84cedec6fe8f05ba93841926fdc72bc4d6a
Zack <168221270+Zval321@users.noreply.github.com>
2024-11-25 21:11:02 -0600

    added an index action to cities

--- src/CitiesPage/CitiesPage.jsx
+++ src/CitiesPage/CitiesPage.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export function CitiesPage () { 
+  const [cities, setCities] = useState([])
   
 
   const handleIndex = async () => {
@@ -9,6 +10,7 @@ export function CitiesPage () {
     try { 
       const response = await axios.get('http://localhost:3000/cities.json')
       console.log(response.data)
+      setCities(response.data)
     } catch(error) { 
       console.error("Error fetching cities", error)
     }
@@ -26,6 +28,23 @@ export function CitiesPage () {
       <h1>
         Your Cities
       </h1>
+        <div>
+          {cities.map((city) => (
+            <div key={city.id}>
+              <h2>{city.name}</h2>
+              <h3>{city.location}</h3>
+              {city.comments.length > 0 ? 
+              <ul>
+                {city.comments.map((comment) => (
+                  <li key={comment.id}>{comment.content}</li>
+                ))}
+              </ul>
+              : ( 
+                <p>No Comments Available</p>
+              )}
+            </div>
+          ))}
+        </div>
     </div>
-  )
-}
+    )
+  }
