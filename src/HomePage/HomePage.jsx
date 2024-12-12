843b3cae96163a844a0259a6b7aa87f1f68045f8
Zack <168221270+Zval321@users.noreply.github.com>
2024-12-12 16:45:41 -0600

    home page functionality

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,6 +1,43 @@
 import './HomePage.css'
+import { useState, useEffect} from 'react'
 
 export function HomePage () { 
+  const [ currentLocation, setCurrentLocation ] = useState({
+    latitude: null, 
+    longitude: null, 
+    error: null
+  })
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCurrentLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            error: null,
+          });
+        },
+        (error) => {
+          setCurrentLocation({
+            latitude: null,
+            longitude: null,
+            error: error.message,
+          });
+        }
+      );
+    } else {
+      setCurrentLocation({ ...location, error: "Geolocation is not supported by this browser." });
+    }
+  }, []);
+
+
+
+
+
+
+
+
   return ( 
     <div>
       <h1>
